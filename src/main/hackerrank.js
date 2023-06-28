071592820889ca63680610884b21c0c18d4b4b3a
Shawn W <78707396+sb-son@users.noreply.github.com>
2023-06-28 15:25:03 -0500

    added test case, comments and executed function to evaluate against the test case

--- src/main/hackerrank.js
+++ src/main/hackerrank.js
@@ -1,4 +1,9 @@
-//compareTriplets
+//compareTriplets problem
+
+//test case
+let a = [5, 6, 7]
+let b = [7, 6, 5]
+//should result in [1,1]
 
 function compareTriplets(a, b) {
     let aliceScore = 0
@@ -15,3 +20,6 @@ function compareTriplets(a, b) {
 
     return arr = [aliceScore, bobScore]
 }
+
+console.log(compareTriplets(a, b));
+
