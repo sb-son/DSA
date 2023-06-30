90b9663a954c8d9255ed01f887ec2ed51920cb43
Shawn W <78707396+sb-son@users.noreply.github.com>
2023-06-30 11:32:20 -0500

    compareTriplets problem completed with java

---
+++ src/main/java/hackerrank.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class hackerrank {
+    public static void main(String[] args) {
+        //compareTriplets problem
+
+        //test case
+        ArrayList<Integer> a = new ArrayList<>(List.of(5, 6, 7));
+        ArrayList<Integer> b = new ArrayList<>(List.of(7, 6, 5));
+        //should result in [1,1]
+
+        System.out.println(compareTriplets(a, b));
+    }
+
+    //compareTriplets problem
+    public static ArrayList<Integer> compareTriplets(ArrayList<Integer> a, ArrayList<Integer> b) {
+        int aliceScore = 0;
+        int bobScore = 0;
+
+        for (int i = 0; i < a.size(); i++) {
+            if (a.get(i) > b.get(i)) {
+                aliceScore++;
+            } else if (a.get(i) < b.get(i)) {
+                bobScore++;
+            }
+        }
+
+        ArrayList<Integer> scores = new ArrayList<>(List.of(aliceScore, bobScore));
+        return scores;
+    }
+}
